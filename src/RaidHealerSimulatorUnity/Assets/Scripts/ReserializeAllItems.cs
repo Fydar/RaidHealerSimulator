a1eda82bd156b6dab68047b0e242a3853105f5dd
fydar <fydar.dev@gmail.com>
2024-06-30 19:08:00 +0100

    📦 Updating Unity version to 2023.2.20f1

---
+++ src/RaidHealerSimulatorUnity/Assets/Scripts/ReserializeAllItems.cs
@@ -0,0 +1,18 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+
+namespace Utility
+{
+	public class ReserializeAllItems : MonoBehaviour
+	{
+	#if UNITY_2017_1_OR_NEWER
+		[MenuItem("Toolkit/Reserialize")]
+		public static void Reserialize()
+		{
+			AssetDatabase.ForceReserializeAssets();
+		}
+	#endif
+	}
+}
+#endif
